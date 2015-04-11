f8e7ca83d5985565a2917ce600e9dc6b0212d35f
Mark Chavez <markchav3z@gmail.com>
2015-04-11 21:26:49 +0800

    fix constant

--- lib/railtie_example/railtie.rb
+++ lib/railtie_example/railtie.rb
@@ -1,7 +1,7 @@
 require "rails"
 require "railtie_example/helper"
 
-class Railtie::Rails::Railtie
+class Railtie < Rails::Railtie
   initializer "railtie_example.action_view" do
     ActiveSupport.on_load(:action_view) do
       include RailtieExample::Helper
